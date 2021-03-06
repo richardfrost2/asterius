248e0f698b38b3d4b42f70fa27747ac230784b02
richardfoy <79873150+richardfrost2@users.noreply.github.com>
2021-03-06 11:09:31 -0700

    Create a better help command
    Based on Icarus's help command. Now hopefully the help command can be more helpful!

--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 class Embed(discord.Embed):
     """Embed with some prebuilt values."""
     def __init__(self):
+        """Creates a new embed."""
         super().__init__()
         self.color = config.embed_color
         self.timestamp = datetime.datetime.now()
